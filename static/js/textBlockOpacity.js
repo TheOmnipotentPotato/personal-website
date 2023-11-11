4c06e71e5cd592a2f84309cc3aeeefd262fa3d5b
TheOmnipotentPotato <jacobnickodemus@Jacobs-MacBook-Pro.local>
2023-11-11 14:44:42 -0700

    updating dead repo

--- static/js/textBlockOpacity.js
+++ static/js/textBlockOpacity.js
@@ -3,7 +3,7 @@ window.onscroll = function() {myFunction()};
 
 function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = 1350 - document.documentElement.clientHeight;
+  var height = 980 - document.documentElement.clientHeight;
   var scrolled = 100 - ((winScroll / height) * 100);
   document.getElementById("home-text-block").style.opacity = scrolled + "%";
 }
